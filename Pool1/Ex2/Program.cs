3e497d2603dbc137eae5e45eb312ad98d5415e56
Alex Damsa <alexdamsa25092001@gmail.com>
2021-01-07 02:34:17 +0200

    added pool1 with some ex

---
+++ Pool1/Ex2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExtensionMethods;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = new int();
+            int b = new int();
+            int c = new int();
+            float x1;
+            float x2;
+
+            #region Getting Input
+
+            Console.Write("a= ");
+            do
+            {
+                a.GetInt();
+            } while (a == 0);
+
+            Console.Write("b= ");
+            b.GetInt();
+
+            Console.Write("c= ");
+            c.GetInt();
+            #endregion
+
+            #region Processing
+
+            int delta = (int)Math.Pow(b, 2) - 4 * a * c;
+
+            x1 = (float)(-b + delta) / (2 * a);
+            x2 = (float)(-b - delta) / (2 * a);
+            #endregion
+
+            #region Writing Output
+
+            Console.WriteLine();
+            Console.WriteLine($"{a}*x^2 + {b}*x + c = 0");
+
+            Console.WriteLine($"=> x1 = {x1}");
+            Console.WriteLine($"=> x2 = {x2}");
+            #endregion
+        }
+    }
+}
---
+++ Pool1/ExtensionMethods/ExtensionMethods.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    public static class ExtensionMethods
+    {
+        public static bool GetInt (this ref int instance)
+        {
+            if (int.TryParse(Console.ReadLine(), out instance))
+            {
+                return true;
+            }
+
+            Console.Write("try again: ");
+            return instance.GetInt();
+        }
+
+        public static int Lenght(this int instance)
+        {
+            if (instance != 0)
+            {
+                return 1 + (instance / 10).Lenght();
+            }
+
+            return 0;
+        }
+    }
+}
