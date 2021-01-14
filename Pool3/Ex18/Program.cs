c6c65447449b7e3291097eb25f2599d1ce39635e
Alex Damsa <alexdamsa25092001@gmail.com>
2021-01-14 14:14:09 +0200

    am uitat sa fac ex 18, dar l am facut acm. da nush daca i bine

--- Pool3/Ex18/Program.cs
+++ Pool3/Ex18/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ExtensionMethods;
 
 namespace Ex18
 {
@@ -10,7 +11,31 @@ namespace Ex18
     {
         static void Main(string[] args)
         {
-            // TODO ex18
+            string input;
+
+            Console.Write("polinom: ");
+            input = Console.ReadLine();
+
+            int n = new int();
+            Console.Write("n= ");
+            n.GetPositiveInt();
+
+            int[] array = new int[n];
+            array.FillArray();
+
+            Console.Write("pozitie: ");
+            n.GetNonNegativeInt();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == 'n')
+                {
+                    input = input.Remove(i, 1);
+                    input = input.Insert(i, array[n].ToString());
+                }
+            }
+
+            Console.WriteLine(input);
         }
     }
 }
